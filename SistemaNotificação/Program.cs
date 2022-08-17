dd1b543fff89f4d7a216391fc82cb3c6d15b9043
Moises Alves <87319078+mooisesalves1@users.noreply.github.com>
2022-08-17 17:50:20 -0300

    Versão 1.0

---
+++ SistemaNotificação/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace SistemaNotificacao
+{
+   class Program
+   {
+       static void Main(string[] args)
+       {
+           Console.WriteLine("*** Sistema de notificação de clientes***");
+           Console.WriteLine("Informe código do cliente:");
+           var CodCliente = Console.ReadLine();
+           Console.WriteLine("Informe mensagem a ser enviada:");
+           var Mensagem = Console.ReadLine();
+           // Simula envio de um email
+           Console.WriteLine("Enviando EMAIL para o cliente {0}",CodCliente);
+           Console.WriteLine("EMAIL: {0}",Mensagem);
+           Console.ReadLine();
+       }
+   }
+}
