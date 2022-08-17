90fee3f5982831871d308e779afe864dd94d2d80
Moises Alves <87319078+mooisesalves1@users.noreply.github.com>
2022-08-17 18:00:32 -0300

    Versão 2.0 - Implementado SMS

--- SistemaNotificação/Program.cs
+++ SistemaNotificação/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+
 namespace SistemaNotificacao
 {
    class Program
@@ -12,10 +13,24 @@ namespace SistemaNotificacao
            var CodCliente = Console.ReadLine();
            Console.WriteLine("Informe mensagem a ser enviada:");
            var Mensagem = Console.ReadLine();
-           // Simula envio de um email
-           Console.WriteLine("Enviando EMAIL para o cliente {0}",CodCliente);
-           Console.WriteLine("EMAIL: {0}",Mensagem);
-           Console.ReadLine();
-       }
+
+            Console.WriteLine("Digite opção de envio, 1-EMAIL ou 2-SMS:");
+            var modoEnvio = Console.ReadLine();
+            if (modoEnvio == "1")
+            {
+                // Simula envio de um email
+                Console.WriteLine("Enviando EMAIL para o cliente {0}", CodCliente);
+                Console.WriteLine("EMAIL: {0}", Mensagem);
+            }
+            else
+                if (modoEnvio == "2")
+                {
+                // Simula envio de um SMS
+                      Console.WriteLine("Enviando SMS para o cliente {0}", CodCliente);
+                      Console.WriteLine("SMS: {0}", Mensagem);
+                }
+                Console.ReadLine();
+
+        }
    }
 }
